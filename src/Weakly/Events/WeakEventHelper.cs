e192b2d9c4cc2b513a51c6468d73a0c664e442fc
Thomas Ibel <tibel@users.noreply.github.com>
2013-10-19 15:48:48 +0200

    begin reflection based WeakEventHandler

--- src/Weakly/Events/WeakEventHelper.cs
+++ src/Weakly/Events/WeakEventHelper.cs
@@ -4,37 +4,48 @@ using System.Reflection;
 
 namespace Weakly
 {
-    /// <summary>
-    /// Helper to register a weak handler to an event.
-    /// </summary>
     internal class WeakEventHelper
     {
-        /// <summary>
-        /// TODO: internal tests only
-        /// </summary>
-        /// <typeparam name="TEventArgs">The type of the event arguments.</typeparam>
-        /// <param name="eventInfo">The event information.</param>
-        /// <param name="sender">The sender.</param>
-        /// <param name="handler">The handler.</param>
-        public static void Add<TEventArgs>(EventInfo eventInfo, object sender, EventHandler<TEventArgs> handler)
+        public static void Add<TEventArgs>(object eventSource, EventInfo eventInfo, Action<object, TEventArgs> handler)
             where TEventArgs : EventArgs
         {
-            // make weak handler and convert to right delegate type
-            var weakAction = new WeakAction<object, TEventArgs>(handler.Target, handler.Method);
+            // create weak handler
+            var weakAction = new WeakAction<object, TEventArgs>(handler);
+            //TODO: support automatic unregister
+
+            // create correct delegate type
             var weakHandler = Delegate.CreateDelegate(
                 eventInfo.EventHandlerType,
                 weakAction,
                 typeof (WeakAction<object, TEventArgs>).GetMethod("Invoke"));
 
-            var addMethod = CompileEventMethod(eventInfo.GetAddMethod(true));
-            addMethod(sender, weakHandler);
+            // register weak handler
+            var addMethod = GetEventMethod(eventInfo.GetAddMethod(true));
+            addMethod(eventSource, weakHandler);
+        }
+
+        private static readonly GenericMethodCache<Action<object, Delegate>> Cache = new GenericMethodCache<Action<object, Delegate>>(); 
+
+        private static Action<object, Delegate> GetEventMethod(MethodInfo method)
+        {
+            var action = Cache.GetValueOrNull(method.MethodHandle);
+            if (action != null) return action;
+            action = CompileEventMethod(method);
+            Cache.AddOrReplace(method.MethodHandle, action);
+            return action;
         }
 
         private static Action<object, Delegate> CompileEventMethod(MethodInfo method)
         {
             var target = Expression.Parameter(typeof(object), "target");
             var handler = Expression.Parameter(typeof(Delegate), "handler");
-            var typedTarget = Expression.Convert(target, method.DeclaringType);
+
+            Expression typedTarget = null;
+            if (!method.IsStatic)
+            {
+                typedTarget = Expression.Convert(target, method.DeclaringType);
+            }
+
             var typedHandler = Expression.Convert(handler, method.GetParameters()[0].ParameterType);
             var body = Expression.Call(typedTarget, method, typedHandler);
             return Expression.Lambda<Action<object, Delegate>>(body, target, handler).Compile();
