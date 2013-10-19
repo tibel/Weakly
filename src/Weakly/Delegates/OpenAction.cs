93f12a84ba08622055848f924db62a7049e8b843
Thomas Ibel <tibel@users.noreply.github.com>
2013-10-19 14:47:07 +0200

    add more OpenAction and OpenFunc

--- src/Weakly/Delegates/OpenAction.cs
+++ src/Weakly/Delegates/OpenAction.cs
@@ -114,5 +114,69 @@ namespace Weakly
             var body = Expression.Call(typedInstance, method, arg1, arg2, arg3);
             return Expression.Lambda<Action<object, T1, T2, T3>>(body, instance, arg1, arg2, arg3).Compile();
         }
+
+        /// <summary>
+        /// Create an open delegate from the specified method.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter of the method that this delegate encapsulates.</typeparam>
+        /// <param name="method">The method.</param>
+        /// <returns>The open delegate.</returns>
+        public static Action<object, T1, T2, T3, T4> From<T1, T2, T3, T4>(MethodInfo method)
+        {
+            var action = Cache.GetValueOrNull<Action<object, T1, T2, T3, T4>>(method.MethodHandle);
+            if (action != null) return action;
+            action = CompileAction<T1, T2, T3, T4>(method);
+            Cache.AddOrReplace(method.MethodHandle, action);
+            return action;
+        }
+
+        private static Action<object, T1, T2, T3, T4> CompileAction<T1, T2, T3, T4>(MethodInfo method)
+        {
+            var instance = Expression.Parameter(typeof(object), "instance");
+            var arg1 = Expression.Parameter(typeof(T1), "arg1");
+            var arg2 = Expression.Parameter(typeof(T2), "arg2");
+            var arg3 = Expression.Parameter(typeof(T3), "arg3");
+            var arg4 = Expression.Parameter(typeof(T4), "arg4");
+
+            var typedInstance = Expression.Convert(instance, method.DeclaringType);
+            var body = Expression.Call(typedInstance, method, arg1, arg2, arg3, arg4);
+            return Expression.Lambda<Action<object, T1, T2, T3, T4>>(body, instance, arg1, arg2, arg3, arg4).Compile();
+        }
+
+        /// <summary>
+        /// Create an open delegate from the specified method.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="T5">The type of the fifth parameter of the method that this delegate encapsulates.</typeparam>
+        /// <param name="method">The method.</param>
+        /// <returns>The open delegate.</returns>
+        public static Action<object, T1, T2, T3, T4, T5> From<T1, T2, T3, T4, T5>(MethodInfo method)
+        {
+            var action = Cache.GetValueOrNull<Action<object, T1, T2, T3, T4, T5>>(method.MethodHandle);
+            if (action != null) return action;
+            action = CompileAction<T1, T2, T3, T4, T5>(method);
+            Cache.AddOrReplace(method.MethodHandle, action);
+            return action;
+        }
+
+        private static Action<object, T1, T2, T3, T4, T5> CompileAction<T1, T2, T3, T4, T5>(MethodInfo method)
+        {
+            var instance = Expression.Parameter(typeof(object), "instance");
+            var arg1 = Expression.Parameter(typeof(T1), "arg1");
+            var arg2 = Expression.Parameter(typeof(T2), "arg2");
+            var arg3 = Expression.Parameter(typeof(T3), "arg3");
+            var arg4 = Expression.Parameter(typeof(T4), "arg4");
+            var arg5 = Expression.Parameter(typeof(T5), "arg5");
+
+            var typedInstance = Expression.Convert(instance, method.DeclaringType);
+            var body = Expression.Call(typedInstance, method, arg1, arg2, arg3, arg4, arg5);
+            return Expression.Lambda<Action<object, T1, T2, T3, T4, T5>>(body, instance, arg1, arg2, arg3, arg4, arg5).Compile();
+        }
     }
 }
--- src/Weakly/Delegates/OpenFunc.cs
+++ src/Weakly/Delegates/OpenFunc.cs
@@ -118,5 +118,71 @@ namespace Weakly
             var body = Expression.Call(typedInstance, method, arg1, arg2, arg3);
             return Expression.Lambda<Func<object, T1, T2, T3, TResult>>(body, instance, arg1, arg2, arg3).Compile();
         }
+
+        /// <summary>
+        /// Create an open delegate from the specified method.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="TResult">The type of the return value of the method that this delegate encapsulates.</typeparam>
+        /// <param name="method">The method.</param>
+        /// <returns>The open delegate</returns>
+        public static Func<object, T1, T2, T3, T4, TResult> From<T1, T2, T3, T4, TResult>(MethodInfo method)
+        {
+            var func = Cache.GetValueOrNull<Func<object, T1, T2, T3, T4, TResult>>(method.MethodHandle);
+            if (func != null) return func;
+            func = CompileFunc<T1, T2, T3, T4, TResult>(method);
+            Cache.AddOrReplace(method.MethodHandle, func);
+            return func;
+        }
+
+        private static Func<object, T1, T2, T3, T4, TResult> CompileFunc<T1, T2, T3, T4, TResult>(MethodInfo method)
+        {
+            var instance = Expression.Parameter(typeof(object), "instance");
+            var arg1 = Expression.Parameter(typeof(T1), "arg1");
+            var arg2 = Expression.Parameter(typeof(T2), "arg2");
+            var arg3 = Expression.Parameter(typeof(T3), "arg3");
+            var arg4 = Expression.Parameter(typeof(T4), "arg4");
+
+            var typedInstance = Expression.Convert(instance, method.DeclaringType);
+            var body = Expression.Call(typedInstance, method, arg1, arg2, arg3, arg4);
+            return Expression.Lambda<Func<object, T1, T2, T3, T4, TResult>>(body, instance, arg1, arg2, arg3, arg4).Compile();
+        }
+
+        /// <summary>
+        /// Create an open delegate from the specified method.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="T5">The type of the fifth parameter of the method that this delegate encapsulates.</typeparam>
+        /// <typeparam name="TResult">The type of the return value of the method that this delegate encapsulates.</typeparam>
+        /// <param name="method">The method.</param>
+        /// <returns>The open delegate</returns>
+        public static Func<object, T1, T2, T3, T4, T5, TResult> From<T1, T2, T3, T4, T5, TResult>(MethodInfo method)
+        {
+            var func = Cache.GetValueOrNull<Func<object, T1, T2, T3, T4, T5, TResult>>(method.MethodHandle);
+            if (func != null) return func;
+            func = CompileFunc<T1, T2, T3, T4, T5, TResult>(method);
+            Cache.AddOrReplace(method.MethodHandle, func);
+            return func;
+        }
+
+        private static Func<object, T1, T2, T3, T4, T5, TResult> CompileFunc<T1, T2, T3, T4, T5, TResult>(MethodInfo method)
+        {
+            var instance = Expression.Parameter(typeof(object), "instance");
+            var arg1 = Expression.Parameter(typeof(T1), "arg1");
+            var arg2 = Expression.Parameter(typeof(T2), "arg2");
+            var arg3 = Expression.Parameter(typeof(T3), "arg3");
+            var arg4 = Expression.Parameter(typeof(T4), "arg4");
+            var arg5 = Expression.Parameter(typeof(T5), "arg5");
+
+            var typedInstance = Expression.Convert(instance, method.DeclaringType);
+            var body = Expression.Call(typedInstance, method, arg1, arg2, arg3, arg4, arg5);
+            return Expression.Lambda<Func<object, T1, T2, T3, T4, T5, TResult>>(body, instance, arg1, arg2, arg3, arg4, arg5).Compile();
+        }
     }
 }
