a5a852ecc664ccecc6fc4c6203ccc07e157b911a
Thomas Ibel <tibel@users.noreply.github.com>
2013-10-29 12:38:13 +0100

    add some more WeakAction and WeakFunc

--- src/Weakly/Delegates/WeakAction.cs
+++ src/Weakly/Delegates/WeakAction.cs
@@ -154,4 +154,92 @@ namespace Weakly
                 OpenAction.From<T1, T2, T3>(Method)(target, arg1, arg2, arg3);
         }
     }
+
+    /// <summary>
+    /// Weak version of <see cref="Action&lt;T1, T2, T3, T4&gt;"/> delegate.
+    /// </summary>
+    /// <typeparam name="T1">The type of the first parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="T2">The type of the second parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="T3">The type of the third parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="T4">The type of the fourth parameter of the method that this delegate encapsulates.</typeparam>
+    public sealed class WeakAction<T1, T2, T3, T4> : WeakDelegate
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeakAction&lt;T1, T2, T3, T4&gt;"/> class.
+        /// </summary>
+        /// <param name="action">The action delegate to encapsulate.</param>
+        public WeakAction(Action<T1, T2, T3, T4> action)
+            : this(action.Target, action.Method)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeakAction&lt;T1, T2, T3, T4&gt;"/> class.
+        /// </summary>
+        /// <param name="target">The class instance on which the current delegate invokes the instance method.</param>
+        /// <param name="method">The method represented by the delegate.</param>
+        public WeakAction(object target, MethodInfo method)
+            : base(target, method)
+        {
+        }
+
+        /// <summary>
+        /// Invokes the method represented by the current weak delegate.
+        /// </summary>
+        /// <param name="arg1">The first parameter of the method that this delegate encapsulates.</param>
+        /// <param name="arg2">The second parameter of the method that this delegate encapsulates.</param>
+        /// <param name="arg3">The third parameter of the method that this delegate encapsulates.</param>
+        /// <param name="arg4">The fourth parameter of the method that this delegate encapsulates.</param>
+        public void Invoke(T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+        {
+            var target = Target;
+            if (target != null)
+                OpenAction.From<T1, T2, T3, T4>(Method)(target, arg1, arg2, arg3, arg4);
+        }
+    }
+
+    /// <summary>
+    /// Weak version of <see cref="Action&lt;T1, T2, T3, T4, T5&gt;"/> delegate.
+    /// </summary>
+    /// <typeparam name="T1">The type of the first parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="T2">The type of the second parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="T3">The type of the third parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="T4">The type of the fourth parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="T5">The type of the fifth parameter of the method that this delegate encapsulates.</typeparam>
+    public sealed class WeakAction<T1, T2, T3, T4, T5> : WeakDelegate
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeakAction&lt;T1, T2, T3, T4, T5&gt;"/> class.
+        /// </summary>
+        /// <param name="action">The action delegate to encapsulate.</param>
+        public WeakAction(Action<T1, T2, T3, T4, T5> action)
+            : this(action.Target, action.Method)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeakAction&lt;T1, T2, T3, T4, T5&gt;"/> class.
+        /// </summary>
+        /// <param name="target">The class instance on which the current delegate invokes the instance method.</param>
+        /// <param name="method">The method represented by the delegate.</param>
+        public WeakAction(object target, MethodInfo method)
+            : base(target, method)
+        {
+        }
+
+        /// <summary>
+        /// Invokes the method represented by the current weak delegate.
+        /// </summary>
+        /// <param name="arg1">The first parameter of the method that this delegate encapsulates.</param>
+        /// <param name="arg2">The second parameter of the method that this delegate encapsulates.</param>
+        /// <param name="arg3">The third parameter of the method that this delegate encapsulates.</param>
+        /// <param name="arg4">The fourth parameter of the method that this delegate encapsulates.</param>
+        /// <param name="arg5">The fifth parameter of the method that this delegate encapsulates.</param>
+        public void Invoke(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+        {
+            var target = Target;
+            if (target != null)
+                OpenAction.From<T1, T2, T3, T4, T5>(Method)(target, arg1, arg2, arg3, arg4, arg5);
+        }
+    }
 }
--- src/Weakly/Delegates/WeakFunc.cs
+++ src/Weakly/Delegates/WeakFunc.cs
@@ -166,4 +166,98 @@ namespace Weakly
             return default(TResult);
         }
     }
+
+    /// <summary>
+    /// Weak version of <see cref="Func&lt;T1, T2, T3, T4, TResult&gt;"/> delegate.
+    /// </summary>
+    /// <typeparam name="T1">The type of the first parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="T2">The type of the second parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="T3">The type of the third parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="T4">The type of the fourth parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="TResult">The type of the return value of the method that this delegate encapsulates.</typeparam>
+    public sealed class WeakFunc<T1, T2, T3, T4, TResult> : WeakDelegate
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeakFunc&lt;T1, T2, T3, T4, TResult&gt;"/> class.
+        /// </summary>
+        /// <param name="function">The function delegate to encapsulate.</param>
+        public WeakFunc(Func<T1, T2, T3, T4, TResult> function)
+            : this(function.Target, function.Method)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeakFunc&lt;T1, T2, T3, T4, TResult&gt;"/> class.
+        /// </summary>
+        /// <param name="target">The class instance on which the current delegate invokes the instance method.</param>
+        /// <param name="method">The method represented by the delegate.</param>
+        public WeakFunc(object target, MethodInfo method)
+            : base(target, method)
+        {
+        }
+
+        /// <summary>
+        /// Invokes the method represented by the current weak delegate.
+        /// </summary>
+        /// <param name="arg1">The first parameter of the method that this delegate encapsulates.</param>
+        /// <param name="arg2">The second parameter of the method that this delegate encapsulates.</param>
+        /// <param name="arg3">The third parameter of the method that this delegate encapsulates.</param>
+        /// <param name="arg4">The fourth parameter of the method that this delegate encapsulates.</param>
+        /// <returns>The return value of the method that this delegate encapsulates.</returns>
+        public TResult Invoke(T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+        {
+            var target = Target;
+            if (target != null)
+                return OpenFunc.From<T1, T2, T3, T4, TResult>(Method)(target, arg1, arg2, arg3, arg4);
+            return default(TResult);
+        }
+    }
+
+    /// <summary>
+    /// Weak version of <see cref="Func&lt;T1, T2, T3, T4, T5, TResult&gt;"/> delegate.
+    /// </summary>
+    /// <typeparam name="T1">The type of the first parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="T2">The type of the second parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="T3">The type of the third parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="T4">The type of the fourth parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="T5">The type of the fifth parameter of the method that this delegate encapsulates.</typeparam>
+    /// <typeparam name="TResult">The type of the return value of the method that this delegate encapsulates.</typeparam>
+    public sealed class WeakFunc<T1, T2, T3, T4, T5, TResult> : WeakDelegate
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeakFunc&lt;T1, T2, T3, T4, T5, TResult&gt;"/> class.
+        /// </summary>
+        /// <param name="function">The function delegate to encapsulate.</param>
+        public WeakFunc(Func<T1, T2, T3, T4, T5, TResult> function)
+            : this(function.Target, function.Method)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeakFunc&lt;T1, T2, T3, T4, T5, TResult&gt;"/> class.
+        /// </summary>
+        /// <param name="target">The class instance on which the current delegate invokes the instance method.</param>
+        /// <param name="method">The method represented by the delegate.</param>
+        public WeakFunc(object target, MethodInfo method)
+            : base(target, method)
+        {
+        }
+
+        /// <summary>
+        /// Invokes the method represented by the current weak delegate.
+        /// </summary>
+        /// <param name="arg1">The first parameter of the method that this delegate encapsulates.</param>
+        /// <param name="arg2">The second parameter of the method that this delegate encapsulates.</param>
+        /// <param name="arg3">The third parameter of the method that this delegate encapsulates.</param>
+        /// <param name="arg4">The fourth parameter of the method that this delegate encapsulates.</param>
+        /// <param name="arg5">The fifth parameter of the method that this delegate encapsulates.</param>
+        /// <returns>The return value of the method that this delegate encapsulates.</returns>
+        public TResult Invoke(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+        {
+            var target = Target;
+            if (target != null)
+                return OpenFunc.From<T1, T2, T3, T4, T5, TResult>(Method)(target, arg1, arg2, arg3, arg4, arg5);
+            return default(TResult);
+        }
+    }
 }
