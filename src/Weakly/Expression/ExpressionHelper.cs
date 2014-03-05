4053b468d1f674d292073cd3d76d385b79e2a9a3
Thomas Ibel <tibel@users.noreply.github.com>
2014-03-05 21:03:12 +0100

    add Validation

---
+++ src/Weakly/Expression/ExpressionHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Weakly
+{
+    /// <summary>
+    /// Extension for <see cref="Expression"/>.
+    /// </summary>
+    public static class ExpressionHelper
+    {
+        /// <summary>
+        /// Converts an expression into a <see cref="MemberInfo"/>.
+        /// </summary>
+        /// <param name="expression">The expression to convert.</param>
+        /// <returns>The member info.</returns>
+        public static MemberInfo GetMemberInfo(this Expression expression)
+        {
+            var lambda = (LambdaExpression)expression;
+
+            MemberExpression memberExpression;
+
+            var unaryExpression = lambda.Body as UnaryExpression;
+            if (unaryExpression != null)
+            {
+                memberExpression = (MemberExpression)unaryExpression.Operand;
+            }
+            else
+            {
+                memberExpression = (MemberExpression)lambda.Body;
+            }
+
+            return memberExpression.Member;
+        }
+    }
+}
