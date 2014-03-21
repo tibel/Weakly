6a58dd7c125ecdc5422fb00cef42ec43b690550f
Thomas Ibel <tibel@users.noreply.github.com>
2014-03-21 21:29:41 +0100

    integrate MVVM parts

--- build/create-package.cmd
+++ build/create-package.cmd
@@ -1,3 +1,2 @@
 @ECHO OFF
 NuGet.exe pack Weakly.nuspec
-NuGet.exe pack Weakly.MVVM.nuspec
--- src/Weakly.MVVM/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Weakly.MVVM")]
-[assembly: AssemblyDescription("Weakly.MVVM adds some basic MVVM features on top of Weakly.")]
