084ac2fbad1b21b7721e02f2c25e2ce27d5cd5b4
Thomas Ibel <tibel@users.noreply.github.com>
2014-03-23 11:59:48 +0100

    add enumerable ForEachAsync() and SelectAsync()

--- src/Weakly/Collections/EnumerableHelper.cs
+++ src/Weakly/Collections/EnumerableHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Weakly
 {
@@ -12,14 +14,39 @@ namespace Weakly
         /// Performs the specified action on each element of the <see cref="IEnumerable&lt;T&gt;"/>.
         /// </summary>
         /// <typeparam name="T">The type of objects to enumerate.</typeparam>
-        /// <param name="enumerable">The enumerable.</param>
+        /// <param name="source">The enumerable.</param>
         /// <param name="action">The <see cref="Action&lt;T&gt;"/> delegate to perform on each element of the <see cref="IEnumerable&lt;T&gt;"/>.</param>
-        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
-            foreach (var item in enumerable)
+            foreach (var item in source)
             {
                 action(item);
             }
+        }
+
+        /// <summary>
+        /// Performs the specified asynchronous action on each element of the <see cref="IEnumerable&lt;T&gt;"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of objects to enumerate.</typeparam>
+        /// <param name="source">The enumerable.</param>
+        /// <param name="asyncAction">The <see cref="Func&lt;T, Task&gt;"/> delegate to perform on each element of the <see cref="IEnumerable&lt;T&gt;"/>.</param>
+        /// <returns>A task that represents the completion.</returns>
+        public static Task ForEachAsync<T>(this IEnumerable<T> source, Func<T, Task> asyncAction)
+        {
+            return Task.WhenAll(source.Select(asyncAction));
+        }
+
+        /// <summary>
+        /// Projects asynchronous each element of a sequence into a new form.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <typeparam name="TResult">The type of the value returned by asyncSelector.</typeparam>
+        /// <param name="source">A sequence of values to invoke a transform function on.</param>
+        /// <param name="asyncSelector">An asynchronous transform function to apply to each element.</param>
+        /// <returns></returns>
+        public static Task<TResult[]> SelectAsync<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Task<TResult>> asyncSelector)
+        {
+            return Task.WhenAll(source.Select(asyncSelector));
         }
     }
 }
