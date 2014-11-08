b82d940eb5ab0b7f5cac19e185269072fb390a98
Thomas Ibel <tibel@users.noreply.github.com>
2014-11-08 14:58:17 +0100

    revert remove of DynamicEvent but mark as obsolete

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Resources;
 
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.2.2.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
---
+++ src/Weakly/Delegates/DynamicEvent.cs
@@ -0,0 +1,32 @@
+﻿﻿using System;
+using System.Reflection;
+
+namespace Weakly
+{
+    /// <summary>
+    /// Helper methods to register or unregister an event handler using reflection.
+    /// </summary>
+    [Obsolete]
+    public static class DynamicEvent
+    {
+        /// <summary>
+        /// Gets the method that adds an event handler to an event source.
+        /// </summary>
+        /// <param name="eventInfo">The event information.</param>
+        /// <returns>The method used to add an event handler delegate to the event source.</returns>
+        public static Action<object, Delegate> GetAddMethod(EventInfo eventInfo)
+        {
+            return eventInfo.AddEventHandler;
+        }
+
+        /// <summary>
+        /// Gets the method that removes an event handler from an event source.
+        /// </summary>
+        /// <param name="eventInfo">The event information.</param>
+        /// <returns>The method used to remove an event handler delegate from the event source.</returns>
+        public static Action<object, Delegate> GetRemoveMethod(EventInfo eventInfo)
+        {
+            return eventInfo.RemoveEventHandler;
+        }
+    }
+}
