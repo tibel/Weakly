21030ebeae37cad477ac0a08dd5705300128a295
Thomas Ibel <tibel@users.noreply.github.com>
2015-04-11 06:21:23 +0200

    add WaitForCompletionStatus

--- src/Weakly/Tasks/TaskHelper.cs
+++ src/Weakly/Tasks/TaskHelper.cs
@@ -198,6 +198,18 @@ namespace Weakly
         #endregion
 
         /// <summary>
+        /// Waits for the task to complete execution, returning the task's final status.
+        /// </summary>
+        /// <param name="task">The task for which to wait.</param>
+        /// <returns>The completion status of the task.</returns>
+        /// <remarks>Unlike Wait, this method will not throw an exception if the task ends in the Faulted or Canceled state.</remarks>
+        public static TaskStatus WaitForCompletionStatus(this Task task)
+        {
+            ((IAsyncResult)task).AsyncWaitHandle.WaitOne();
+            return task.Status;
+        }
+
+        /// <summary>
         /// Don't await the supplied task. Use this inside async methods to avoid compiler warnings.
         /// </summary>
         /// <param name="task">The Task to be monitored.</param>
